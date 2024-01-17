aa5c50a7d8429b22bce619719bcbbddef15d6219
mtenyg24 <m.gatluak@alustudent.com>
2024-01-17 23:59:42 +0300

    hello world

---
+++ hello_world-app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World App'),
+        ),
+        body: 
+        const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
