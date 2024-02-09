83fe886edff988cb3d146a529f84a126f4a697f6
mtenyg24 <m.gatluak@alustudent.com>
2024-02-10 00:03:03 +0300

    change

--- afya_chapchap/lib/main.dart
+++ afya_chapchap/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './Screens/meeting_screen.dart';
+import './Screens/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainScreen(),
+      home: SignUpPage(),
     );
   }
 }
