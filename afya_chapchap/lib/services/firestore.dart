b61c1de00beefcd9716cfadc3f296974ea0b7e49
Vulgence <e.kinyua@alustudent.com>
2024-02-27 23:22:53 +0300

    CRUD comments

--- afya_chapchap/lib/services/firestore.dart
+++ afya_chapchap/lib/services/firestore.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
+    // get collection of appointments
   final CollectionReference appointments =
       FirebaseFirestore.instance.collection('appointments');
 
+//CREATE: add a new appointment
   Future<void> addAppointment(
     String name,
     String description,
@@ -19,6 +21,7 @@ class FirestoreService {
     });
   }
 
+ //READ: get appointments from the database
   Stream<QuerySnapshot> getAppointmentsStream() {
     final appointmentsStream =
         appointments.orderBy('timestamp', descending: true).snapshots();
@@ -26,6 +29,7 @@ class FirestoreService {
     return appointmentsStream;
   }
 
+  //UPDATE: update appointments given a doc id
   Future<void> updateAppointment(
     String docID,
     String name,
@@ -42,6 +46,7 @@ class FirestoreService {
     });
   }
 
+  //DELETE: delete appointments given a doc id
   Future<void> deleteAppointment(String docID) {
     return appointments.doc(docID).delete();
   }
