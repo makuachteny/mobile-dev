0f35be92e8798e9a7cf6d74e7ee915d16b43dd66
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-04-04 00:26:08 +0300

    profilepage updates

--- afya_chapchap/lib/Screens/profile.dart
+++ afya_chapchap/lib/Screens/profile.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: use_build_context_synchronously, avoid_print
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -20,7 +21,11 @@ Future<void> _initializeFirebase() async {
 
 class ProfilePage extends StatefulWidget {
   final void Function(String profileImageUrl, String fullName) onUpdateProfile;
-  const ProfilePage({super.key, required this.onUpdateProfile, required Null Function(String profileImageUrl, String fullName) updateProfile});
+  
+  const ProfilePage({
+    super.key,
+    required this.onUpdateProfile, required Null Function(String profileImageUrl, String fullName) updateProfile,
+  });
 
   @override
   // ignore: library_private_types_in_public_api
@@ -47,55 +52,54 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
-    Future<void> _fetchUserProfile() async {
-  try {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? fullName = prefs.getString('fullName');
-    String? profileImageUrl = prefs.getString('profileImageUrl');
+  Future<void> _fetchUserProfile() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? fullName = prefs.getString('fullName');
+      String? profileImageUrl = prefs.getString('profileImageUrl');
 
-  
-    // fetch image from the database
-    if (profileImageUrl == null) {
-      String? userId = await _profileCollection.getCurrentUserId();
-      Map<String, dynamic>? userProfile =
-          await _profileCollection.getUserProfile(userId!);
+      // fetch image from the database
+      if (profileImageUrl == null) {
+        String? userId = await _profileCollection.getCurrentUserId();
+        Map<String, dynamic>? userProfile =
+            await _profileCollection.getUserProfile(userId!);
 
-      // fetch profile details from database
-      if (userProfile != null) {
+        // fetch profile details from database
+        if (userProfile != null) {
+          setState(() {
+            _fullNameController.text = userProfile['fullName'] ?? '';
+            _ageController.text =
+                userProfile['age'] != null ? userProfile['age'].toString() : '';
+            _locationController.text = userProfile['location'] ?? '';
+            _medicalConditionsController.text =
+                userProfile['medicalConditions'] ?? '';
+            _passwordController.text = ''; // Clear password field
+            _profileImageUrl = userProfile['profileImageUrl'];
+
+            // Store fetched profile details in SharedPreferences
+            prefs.setString('fullName', fullName!);
+            prefs.setString('profileImageUrl', _profileImageUrl!);
+            prefs.setString('age', _ageController.text);
+            prefs.setString('location', _locationController.text);
+            prefs.setString(
+                'medicalConditions', _medicalConditionsController.text);
+          });
+        }
+      } else {
         setState(() {
-          _fullNameController.text = userProfile['fullName'] ?? '';
-          _ageController.text =
-              userProfile['age'] != null ? userProfile['age'].toString() : '';
-          _locationController.text = userProfile['location'] ?? '';
+          _fullNameController.text = fullName ?? '';
+          _profileImageUrl = profileImageUrl;
+          _ageController.text = prefs.getString('age') ?? '';
+          _locationController.text = prefs.getString('location') ?? '';
           _medicalConditionsController.text =
-              userProfile['medicalConditions'] ?? '';
-          _passwordController.text = ''; // Clear password field
-          _profileImageUrl = userProfile['profileImageUrl'];
-
-          // Store fetched profile details in SharedPreferences
-          prefs.setString('fullName', fullName!);
-          prefs.setString('profileImageUrl', _profileImageUrl!);
-          prefs.setString('age', _ageController.text);
-          prefs.setString('location', _locationController.text);
-          prefs.setString('medicalConditions', _medicalConditionsController.text);
-        });
-      }
-    } else {
-      setState(() {
-        _fullNameController.text = fullName ?? '';
-        _profileImageUrl = profileImageUrl;
-        _ageController.text = prefs.getString('age') ?? '';
-        _locationController.text = prefs.getString('location') ?? '';
-        _medicalConditionsController.text = prefs.getString('medicalConditions') ?? '';
+              prefs.getString('medicalConditions') ?? '';
         });
       }
     } catch (e) {
-      
       print('Error fetching user profile: $e');
       // Handle error
     }
   }
-
 
   Future<void> _updateProfile() async {
     try {
@@ -115,23 +119,22 @@ class _ProfilePageState extends State<ProfilePage> {
         throw Exception('Current user ID is null');
       }
 
-    await _profileCollection.updateProfile(
-      fullName: fullName,
-      age: age,
-      location: location,
-      medicalConditions: medicalConditions,
-      password: password,
-      imageUrl: imageUrl,
-      userId: userId,
-      updatedFullName: fullName,
-      updatedAge: age,
-      updatedLocation: location,
-      updatedMedicalConditions: medicalConditions,
-      updatedPassword: password,
-      updatedImageUrl: imageUrl,
-    );
+      await _profileCollection.updateProfile(
+        fullName: fullName,
+        age: age,
+        location: location,
+        medicalConditions: medicalConditions,
+        password: password,
+        imageUrl: imageUrl,
+        userId: userId,
+        updatedFullName: fullName,
+        updatedAge: age,
+        updatedLocation: location,
+        updatedMedicalConditions: medicalConditions,
+        updatedPassword: password,
+        updatedImageUrl: imageUrl,
+      );
 
-    // After successfully updating the profile
       widget.onUpdateProfile(imageUrl, fullName);
 
       SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -141,10 +144,7 @@ class _ProfilePageState extends State<ProfilePage> {
       prefs.setString('location', location);
       prefs.setString('medicalConditions', medicalConditions);
 
-      // After successfully updating the profile
-    widget.onUpdateProfile(imageUrl, fullName);
-
-    ScaffoldMessenger.of(context).showSnackBar(
+      ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Profile updated successfully')),
       );
     } catch (e) {
@@ -165,18 +165,19 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('Profile'),
-      backgroundColor: Colors.blue,
-    ),
-    body: ListView(
-      padding: const EdgeInsets.all(20.0),
-      children: [
-            GestureDetector(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: GestureDetector(
               onTap: () {
                 showModalBottomSheet(
                   context: context,
@@ -206,99 +207,79 @@ Widget build(BuildContext context) {
                   },
                 );
               },
-              child: Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.secondary,
-                        width: 2,
-                      ),
-                    ),
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.white,
-                      backgroundImage: _image != null
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.secondary,
+                    width: 2,
+                  ),
+                ),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  backgroundImage: _image != null
                       ? FileImage(_image!)
                       : _profileImageUrl != null
-                      ?NetworkImage(_profileImageUrl!)
-                      :null as ImageProvider<Object>?,
-                    ),
-                  ),                
-
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blue,
-                      ),
-                      padding: const EdgeInsets.all(5),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                ],
+                          ? NetworkImage(_profileImageUrl!)
+                          : null as ImageProvider<Object>?,
+                ),
               ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              'Update Profile Details',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.blue[900],
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Update Profile Details',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blue[900],
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _fullNameController,
-              decoration: const InputDecoration(
-                hintText: 'Full Name',
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _fullNameController,
+            decoration: const InputDecoration(
+              hintText: 'Full Name',
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _ageController,
-              decoration: const InputDecoration(
-                hintText: 'Age',
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _ageController,
+            decoration: const InputDecoration(
+              hintText: 'Age',
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _locationController,
-              decoration: const InputDecoration(
-                hintText: 'Location',
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _locationController,
+            decoration: const InputDecoration(
+              hintText: 'Location',
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _medicalConditionsController,
-              decoration: const InputDecoration(
-                hintText: 'Your Existing Medical Conditions',
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _medicalConditionsController,
+            decoration: const InputDecoration(
+              hintText: 'Your Existing Medical Conditions',
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Update Password',
-              ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'Update Password',
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _updateProfile,
-              child: const Text('Update Profile'),
-            ),
-          ],
-        ),
-      );
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _updateProfile,
+            child: const Text('Update Profile'),
+          ),
+        ],
+      ),
+    );
   }
 }
